ba20fb0e6b674acf14d5d63d8270c9d50bd3e7d5
luciaicull <lj2141301@gmail.com>
2020-09-18 18:42:07 +0900

    split xml-midi key and midi-midi key

--- feature_generator/constant.py
+++ feature_generator/constant.py
@@ -1,6 +1,12 @@
 ALIGN_DIR = '/home/yoojin/repositories/AlignmentTool_v190813'
 
-FEATURE_LIST = ['velocity', 
-                'original_duration', 
-                'elongated_duration',
-                'interval']
+MIDI_MIDI_FEATURE_LIST = ['velocity', 
+                          'original_duration', 
+                          'elongated_duration',
+                          'interval']
+
+XML_MIDI_FEATURE_LIST = ['beat_tempo',
+                         'measure_tempo',
+                         'velocity',
+                         'original_duration',
+                         'elongated_duration']
