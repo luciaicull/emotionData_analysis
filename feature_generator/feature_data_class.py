f33692d19f9d85dd9c0a5880df03655ed04aebb1
luciaicull <lj2141301@gmail.com>
2020-10-11 17:56:41 +0900

    init feature data class which contains each features from feature_extraction and add normalization methods

--- feature_generator/feature_data_class.py
+++ feature_generator/feature_data_class.py
@@ -1,4 +1,8 @@
 from tqdm import tqdm
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, RobustScaler
+from matplotlib import pyplot as plt
+from pathlib import Path
+
 
 class FeatureData:
     def __init__(self, set_name, performance_data):
@@ -6,9 +10,11 @@ class FeatureData:
         self.emotion_number = performance_data.emotion_number
         self.performance_data = performance_data
         self.feature_data = dict()
+        self.scaled_feature_data = dict()
 
     def make_features_into_dict(self):
         pass
+
 
 class FeatureDataset:
     def __init__(self, set_list):
@@ -18,39 +24,53 @@ class FeatureDataset:
                 set_dict['data_list'] => list of FeatureData class
         '''
         self.set_list = self._init_set_list(set_list)
-    
+
     def _init_set_list(self, raw_set_list):
         set_list = []
-        for set_dict in tqdm(raw_set_list):
+        for set_dict in raw_set_list:
             set_name = set_dict['name']
             performance_data_list = set_dict['list']
 
-            new_dict = {'name': set_name, 'data_list':[]}
+            new_dict = {'name': set_name, 'data_list': []}
             for performance_data in performance_data_list:
                 new_dict['data_list'].append(FeatureData(set_name, performance_data))
-                # set_list.append(FeatureData(set_name, performance_data))
 
             set_list.append(new_dict)
         return set_list
-    
-    def get_final_data(self, split):
+
+    def get_final_data(self, split, hop):
         for set_dict in self.set_list:
             set_name = set_dict['name']
             data_list = set_dict['data_list']
-            for feature_data in data_list:
-                feature_data.split_into_measures(split)
-            
-        self._split_into_measures(split)
-        self._get_statistics()
+            # 1. get set-level normalized features
+            self._normalize_set_level(data_list)
+
+            # 2. split
+
+            # 3. get statistics
+
+        # 4. return in dict format
         return self._convert_to_dict()
 
+    def _normalize_set_level(self, data_list):
+        feature_keys = data_list[0].feature_data.keys()
+        for key in tqdm(feature_keys):
+            indices = [0]
+            total_feat_list = []
+            for eN_class in data_list:
+                feature_list = [[feat] for feat in eN_class.feature_data[key]]
+                indices.append(indices[-1]+len(feature_list))
+                total_feat_list += feature_list
 
-    def _split_into_measures(self, split):
-        pass
-
-    def _get_statistics(self):
-        pass
-
+            scaler = StandardScaler()
+            scaler.fit(total_feat_list)
+            transformed = scaler.transform(total_feat_list)
+            for i, eN_class in enumerate(data_list):
+                feature_list = [feat[0] for feat in transformed[indices[i]:indices[i+1]]]
+                eN_class.scaled_feature_data[key] = feature_list
+            
     def _convert_to_dict(self):
-        pass
+        # TODO
+        return
+    
     
