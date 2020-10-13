33b4c20b0c1154bf56e11fadb4841ed9ebb9e1be
luciaicull <lj2141301@gmail.com>
2020-10-13 22:30:18 +0900

    finalize generation main method

--- feature_generator/generate_data.py
+++ feature_generator/generate_data.py
@@ -2,7 +2,7 @@
 from .arg_parser import get_parser
 from .raw_data_class import MidiMidiDataset, XmlMidiDataset
 from .feature_extraction import MidiMidiFeatureExtractor, XmlMidiFeatureExtractor
-from .constant import MIDI_MIDI_FEATURE_LIST, XML_MIDI_FEATURE_LIST
+from .constant import MIDI_MIDI_FEATURE_LIST, XML_MIDI_FEATURE_LIST, FEATURE_LIST_TMP
 from . import utils
 
 def generate():
@@ -23,14 +23,16 @@ def generate():
     else:
         # score - midi matching
         dataset = XmlMidiDataset(emotion_path)
-        utils.save_datafile(emotion_save_path, 'xml_midi_matched.dat', dataset)
-        dataset = utils.load_datafile(emotion_save_path, 'xml_midi_matched.dat')
+        utils.save_datafile(emotion_save_path, 'raw_dataset.dat', dataset)
+        dataset = utils.load_datafile(emotion_save_path, 'raw_dataset.dat')
         # extract features
-        extractor = XmlMidiFeatureExtractor(dataset.set_list, XML_MIDI_FEATURE_LIST, split=16)
+        extractor = XmlMidiFeatureExtractor(dataset.set_list, XML_MIDI_FEATURE_LIST)
     
-    feature_data = extractor.extract_features()
-    
-    utils.save_datafile(emotion_save_path, args.save_name, feature_data)
+    feature_dataset = extractor.extract_features()
+    utils.save_datafile(emotion_save_path, 'feature_dataset.dat', feature_dataset)
+    #feature_dataset = utils.load_datafile(emotion_save_path, 'feature_dataset.dat')
+    final_feature_data = feature_dataset.get_final_data(split=8, hop=1)
+    utils.save_datafile(emotion_save_path, args.save_name, final_feature_data)
 
 
 
