041e5a3761463512092642df5790a1bd7575c815
luciaicull <lj2141301@gmail.com>
2020-10-13 23:04:01 +0900

    revise make_X_Y for final feature data format

--- feature_analysis/utils.py
+++ feature_analysis/utils.py
@@ -13,23 +13,19 @@ def load_datafile(path, name):
         data = u.load()
     return data
 
-def make_X_Y(feature_data):
+def make_X_Y(total_dataset):
     x = []
     y = []
-    exist_keys = list(feature_data[0]['set'][0]['scaled_stats'].keys())
-    feature_key_list = [key for key in exist_keys if ('_diff' in key) or ('_ratio' in key) or ('relative_' in key)]
+    feature_key_list = [key for key in list(total_dataset[0]['total_scaled_statistics'].keys())
+                            if ('_diff' in key) or ('_ratio' in key) or ('relative_' in key)]
 
-    for emotion_set in feature_data:
-        set_name = emotion_set['name']
-        feature_set_list = emotion_set['set']
-        for feature_set in feature_set_list:
-            emotion_number = feature_set['emotion_number']
-            scaled_stats = feature_set['scaled_stats']
-            
-            data = []
-            for key in feature_key_list:
-                data.append(scaled_stats[key])
-            x.append(data)
-            y.append(emotion_number)
+    for feature_data in total_dataset:
+        emotion_number = feature_data['emotion_number']
+        total_scaled_statistics = feature_data['total_scaled_statistics']
 
+        data = []
+        for key in feature_key_list:
+            data.append(total_scaled_statistics[key])
+        x.append(data)
+        y.append(emotion_number)
     return np.array(x), np.array(y), feature_key_list
