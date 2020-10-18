b774a8efb8d10280dff8f45333f9f2597bb7fbc3
luciaicull <lj2141301@gmail.com>
2020-10-19 01:35:28 +0900

    revise data format into final version

--- feature_analysis/utils.py
+++ feature_analysis/utils.py
@@ -16,16 +16,16 @@ def load_datafile(path, name):
 def make_X_Y(total_dataset):
     x = []
     y = []
-    feature_key_list = [key for key in list(total_dataset[0]['total_scaled_statistics'].keys())
+    feature_key_list = [key for key in list(total_dataset[0]['scaled_statistics'].keys())
                             if ('_diff' in key) or ('_ratio' in key) or ('relative_' in key)]
 
     for feature_data in total_dataset:
         emotion_number = feature_data['emotion_number']
-        total_scaled_statistics = feature_data['total_scaled_statistics']
+        scaled_statistics = feature_data['scaled_statistics']
 
         data = []
         for key in feature_key_list:
-            data.append(total_scaled_statistics[key])
+            data.append(scaled_statistics[key])
         x.append(data)
         y.append(emotion_number)
     return np.array(x), np.array(y), feature_key_list
