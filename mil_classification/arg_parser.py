fdcd70f324834c032f548c57b27f4fbf2d7e110e
luciaicull <lj2141301@gmail.com>
2020-10-29 17:17:35 +0900

    remove unused argument

--- mil_classification/arg_parser.py
+++ mil_classification/arg_parser.py
@@ -7,11 +7,8 @@ def get_parser():
 
     p.add_argument("--path", type=Path,
                    default="/home/yoojin/data/emotionDataset/final/save/", help="emotion data folder name")
-    p.add_argument("--frag_data_name", type=str,
+    p.add_argument("--data_name", type=str,
                    default="splitted_hop_8_split_16.dat")
-    p.add_argument("--total_data_name", type=str,
-                   default="raw_feature_dataset_dict.dat")
-
 
     p.add_argument("--learning_rate", type=float,
                    default=0.001)
