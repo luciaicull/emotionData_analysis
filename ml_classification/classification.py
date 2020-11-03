ede5ddb3531080524d8aa533db1b31f0047cd181
luciaicull <lj2141301@gmail.com>
2020-11-04 00:50:02 +0900

    remove comments

--- ml_classification/classification.py
+++ ml_classification/classification.py
@@ -15,23 +15,6 @@ def main():
     r = Runner(total_data, train_data, test_data)
     r.run_svm()
 
-    print('')
-'''
-def fragment_main():
-    dir_path = Path('/home/yoojin/data/emotionDataset/final/save')
 
-    total_data = utils.load_datafile(dir_path, 'xml_midi_no_split_feature_data.dat')
-    fragment_data = utils.load_datafile(dir_path, 'xml_midi_8measure_split_feature_data.dat')
-
-    _, train_data, test_data = utils.split_train_test(total_data)
-
-    r = Runner(None, train_data, test_data)
-    r._run_train_test_svm()
-
-    r = Runner(None, total_data, fragment_data)
-    r.test_fragment()
-
-    print('')
-'''
 if __name__ == "__main__":
     main()
